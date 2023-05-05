856c65034e2b3a3a240cfdb21b8cc0049e9318a8
多听多看多吃饭 <92104787+soarbamboo@users.noreply.github.com>
2023-05-05 15:29:29 +0800

    Update FileServiceImpl.java

--- src/main/java/com/example/iceserver/service/Impl/FileServiceImpl.java
+++ src/main/java/com/example/iceserver/service/Impl/FileServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.iceserver.service.Impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.iceserver.dto.FileDto;
 import com.example.iceserver.entity.File;
@@ -11,35 +12,48 @@ import com.google.gson.Gson;
 import com.qiniu.common.QiniuException;
 import com.qiniu.common.Zone;
 import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.BatchStatus;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements FileService {
     private UploadManager uploadManager;
 
+    private BucketManager bucketManager;
+
     private String token;
 
     private Auth auth;
 
+    public FileServiceImpl() {
+        init();
+    }
 
     private void init() {
         // 构造一个带指定Zone对象的配置类, 注意这里的Zone.zone0需要根据主机选择
         Configuration cf = new Configuration(Zone.zone1());
+        // 创建一个上传管理类
         uploadManager = new UploadManager(cf);
         auth = Auth.create(QiniuUtils.ASSESS_KEY ,QiniuUtils.SECRET_KEY);
         // 根据命名空间生成的上传token
         token = auth.uploadToken(QiniuUtils.BUCKET);
+        // 创建一个空间管理类
+        bucketManager = new BucketManager(auth,cf);
     }
     @Override
     public FileDto uploadQiniuFile(InputStream file, String path) {
-        init();
         try {
             Response res = uploadManager.put(file,path,token,null,null);
             if(!res.isOK()){
@@ -47,7 +61,7 @@ public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements Fi
             }
             FileDto fileDto = new FileDto();
             DefaultPutRet putRet = new Gson().fromJson(res.bodyString(),DefaultPutRet.class);
-            Boolean  aBoolean = saveImage(putRet);
+            Boolean  aBoolean = saveFile(putRet);
             if(aBoolean){
                 fileDto.setKey(putRet.hash);
                 fileDto.setPath(QiniuUtils.DOMAIN + "/"+putRet.key);
@@ -59,8 +73,33 @@ public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements Fi
         }
         return null;
     }
+    @Override
+    public Map<String, Object> gitFileList (int current, int pageSize) {
+        QueryWrapper<File> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("update_time");
+        // 分页构造器
+        Page<File> pageInfo = new Page<>(current,pageSize);
+        // 执行查询
+        this.page(pageInfo,queryWrapper);
 
-    private Boolean saveImage(DefaultPutRet putRet){
+        // 分页的数据
+        List<File> data = pageInfo.getRecords();
+            // 条数
+        long total = pageInfo.getTotal();
+        // 页数
+        long pagesize = pageInfo.getPages();
+        // 当前页数
+        long currents = pageInfo.getCurrent();
+        Map<String,Object> map = new HashMap<>();
+        map.put("data",data);
+        map.put("total",total);
+        map.put("pageSize",pagesize);
+        map.put("current",currents);
+        return map;
+    }
+
+
+    private Boolean saveFile(DefaultPutRet putRet){
         // 新建查询条件
         QueryWrapper<File> queryWrapper = new QueryWrapper<>();
         // 先判断hash的唯一性
@@ -75,4 +114,35 @@ public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements Fi
        this.save(file);
        return true;
     }
+
+    @Override
+    public Boolean deleteQiniuFile(String[] keyList) {
+        try {
+            BucketManager.BatchOperations batchOperations = new BucketManager.BatchOperations();
+            batchOperations.addDeleteOp(QiniuUtils.BUCKET, keyList);
+            Response response = bucketManager.batch(batchOperations);
+            BatchStatus[] batchStatusList = response.jsonToObject(BatchStatus[].class);
+            for (int i = 0; i < keyList.length; i++) {
+                BatchStatus status = batchStatusList[i];
+                String key = keyList[i];
+                System.out.print(key + "\t");
+                if (status.code == 200) {
+                    System.out.println("delete success");
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }catch (QiniuException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean deleteFile(String[] ids) {
+        this.removeByIds(Arrays.asList(ids));
+        return null;
+    }
+
 }
